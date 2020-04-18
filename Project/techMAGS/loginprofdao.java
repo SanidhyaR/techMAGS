3b7ab5dd55275db7f28e5f1c62d80c8b07beb1ae
SanidhyaR <62399284+SanidhyaR@users.noreply.github.com>
2020-04-18 19:24:23 +0530

    Add files via upload

---
+++ Project/techMAGS/loginprofdao.java
@@ -0,0 +1,14 @@
+package com.techmags_prof;
+
+public class loginprofdao {
+			
+		public boolean check(String uname, String password) {
+			
+			//Connect Driver in try catch 
+			//get usernames passwords and give result
+			
+			
+			return false;	
+		}
+
+}
